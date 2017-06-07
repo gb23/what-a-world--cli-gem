79af23537a9bf74772da662110f61023e05285f5
gb23 <gregbenj@hotmail.com>
2017-06-07 15:08:09 -0700

    Add require_all

--- lib/what_a_world.rb
+++ lib/what_a_world.rb
@@ -2,6 +2,11 @@
 puts "found what_a_world.rb"
 require 'nokogiri'
 require 'open-uri'
-require_relative "what_a_world/version"
-require_relative "what_a_world/cli"
+require 'require_all'
+# require_relative "what_a_world/version"
+# require_relative "what_a_world/cli"
+# require_relative "what_a_world/scraper"
+# require_relative "what_a_world/country"
+# require_relative "what_a_world/issues"
+require_all './lib/what_a_world'
 
