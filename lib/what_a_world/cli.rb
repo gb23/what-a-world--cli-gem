2794dcd50bddd254a63878b1b683efccf9efd349
gb23 <gregbenj@hotmail.com>
2017-06-07 16:08:15 -0700

    class ScraperCli #initialize, #all

--- lib/what_a_world/cli.rb
+++ lib/what_a_world/cli.rb
@@ -1,17 +1,19 @@
 class WhatAWorld::CLI
     def call
         letter = welcome
+        scraper = WhatAWorld::Scraper::ScraperCli.new(letter)
+        #scraper.all => returns array of letter of country
     end
 
     def welcome
         puts "Welcome!"
         letter = nil
-        while !(/[A-Z]/.match(letter))
+        while !(/\A[A-Z]\z/.match(letter))
             puts "Select A-Z"
             print ":"
-            letter = gets.strip.upcase
-            letter
-        end
+            letter = gets.strip.upcase 
+        end 
+        letter
     end
 
 end
--- lib/what_a_world/scraper.rb
+++ lib/what_a_world/scraper.rb
@@ -1,5 +1,14 @@
 class WhatAWorld::Scraper
     class ScraperCli
+        attr_accessor :letter :all
+        def initialize(letter)
+            @letter = letter
+            @all = []
+        end
+
+        def all
+            @all
+        end
     end
 
     class ScraperCountry
