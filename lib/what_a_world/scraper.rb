f7b231fcda520ce912710c5638d3949c8700969a
gb23 <gregbenj@hotmail.com>
2017-06-07 22:49:34 -0700

    modify ScraperCli class

--- lib/what_a_world/scraper.rb
+++ lib/what_a_world/scraper.rb
@@ -2,11 +2,15 @@ class WhatAWorld::Scraper
     class ScraperCli
         @@url = "https://www.cia.gov/library/publications/the-world-factbook/"
         
-        attr_accessor :letter, :all_countries, :url_extensions, :countries
+        attr_accessor :letter, :all_countries, :all_url_extensions
         def initialize(letter)
             @letter = letter
             @all_countries = []
-            @url_extensions = []
+            @all_url_extensions = []
+            gather
+        end
+
+        def gather
             self.find_all_countries
             self.find_countries_by_letter
         end
@@ -15,20 +19,12 @@ class WhatAWorld::Scraper
             html = open(@@url)
             all_countries_page = Nokogiri::HTML(html)
             country_names = ""
-            country_url_extensions = []
             country_names = all_countries_page.css(".selecter_links option[value^='geos']").text  #"-World--Euro-"]
             self.all_countries = country_names.split("  ")
-            self.url_extensions = all_countries_page.css(".selecter_links option").collect{ |link|
+            self.all_url_extensions = all_countries_page.css(".selecter_links option").collect{ |link|
                 link.attr('value') unless link.attr('value') === ""
             }
-            self.url_extensions.reject!{|url| url.nil?}
-
-        end
-
-        def find_countries_by_letter
-
-
-
+            self.all_url_extensions.reject!{|url| url.nil?}
         end
     end
 
